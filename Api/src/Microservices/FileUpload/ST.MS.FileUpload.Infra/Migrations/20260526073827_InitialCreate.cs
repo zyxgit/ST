0cd1d511a8a31748e95ad5be45351c4bf4f949c9
zzzz_zyx <2515832617@qq.com>
2026-05-26 15:46:29 +0800

    添加各个项目的迁移

---
+++ Api/src/Microservices/FileUpload/ST.MS.FileUpload.Infra/Migrations/20260526073827_InitialCreate.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ST.MS.FileUpload.Infra.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "files",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    file_name = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: false),
+                    file_path = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: false),
+                    file_size = table.Column<long>(type: "bigint", nullable: false),
+                    content_type = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: false),
+                    extension = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: false),
+                    access_level = table.Column<int>(type: "integer", nullable: false),
+                    uploader_name = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: true),
+                    create_by = table.Column<Guid>(type: "uuid", nullable: false),
+                    create_time = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_files", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "files");
+        }
+    }
+}
